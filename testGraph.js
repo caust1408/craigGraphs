6173eba3852d2af6398eba2aa0802e7f57b1efc8
Craig <cav9871@saintjoe.edu>
2015-03-16 10:59:59 -0400

    I forgot to add in my own dataset, So I just added it in

--- testGraph.js
+++ testGraph.js
@@ -1,11 +1,13 @@
 load("Graph-full.js");
 g = new Graph(6);
-g.addEdge(1,2);
-g.addEdge(2,5);
-g.addEdge(1,3);
-g.addEdge(1,4);
 g.addEdge(0,1);
-g.vertexList = ["CS1", "CS2", "Data Structures", "Assembley Language", "Operating Systems", "Algorithms"];
+g.addEdge(1,2);
+g.addEdge(1,3);
+g.addEdge(3,4);
+g.addEdge(3,5);
+//g.addEdge(5,6);
+
+g.vertexList = ["Get Beans", "Grind Beans", "Clean French Press", "Boil Water", "Put coffee grounds in", "Pour Water in"];
 g.showGraph();
 //g.bfs(0);
 //var vertex = 4;
@@ -14,3 +16,4 @@ g.showGraph();
 //g.showPath(paths);
 print();
 g.topSort();
+
