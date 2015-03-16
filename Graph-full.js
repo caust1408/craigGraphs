27a2b1e789c447837ac9e93f2bd6c3da79c98551
Craig <cav9871@saintjoe.edu>
2015-03-16 09:17:30 -0400

    Added the remaining details for graph class

--- Graph-full.js
+++ Graph-full.js
@@ -17,12 +17,44 @@ function Graph(v) {
   this.showPath = showPath;
   this.bfs = bfs;
   this.edgeTo = [];
+  this.topSortHelper = topSortHelper;
+  this.topSort = topSort;
 }
 
 function addEdge(v,w) {
   this.adj[v].push(w);
   this.adj[w].push(v);
   this.edges++;
+}
+
+function topSort() {
+	var stack = [];
+	var visited = [];
+	for (var i = 0; i < this.vertices; i++) {
+		visited[i] = false;
+	}
+	for (var i = 0; i < this.vertices; i++) {
+		if (!visited[i]) {
+			this.topSortHelper(i, visited, stack);
+		}
+	}
+	for (var i = 0; i < stack.length; i++) {
+		if(stack[i] !== undefined && stack[i] !== false) {
+			print(this.vertexList[stack[i]]);
+		}
+	}
+}
+
+function topSortHelper(v,visited, stack) {
+	visited[v] = true;
+	for (var i = 0; i < this.adj[v]; i++) {
+		var w = this.adj[v][i];
+		if (!visited[w]) {
+		
+			this.topSortHelper(visited[w], visited, stack);
+		}
+	}
+	stack.push(v);
 }
 
 function showGraph() {
