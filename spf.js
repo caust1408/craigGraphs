69973c2573405a85f0de3779b42a4bb835914a34
Craig <cav9871@saintjoe.edu>
2015-04-02 15:04:06 -0400

    I added in my graph for the exam

--- spf.js
+++ spf.js
@@ -1,18 +1,17 @@
 
 load("Graph-full.js");
-g = new Graph(10);
+g = new Graph(6);
 g.addEdge(0,1);
+g.addEdge(0,2);
+g.addEdge(1,2);
+g.addEdge(1,3);
 g.addEdge(1,4);
-g.addEdge(4,2);
+g.addEdge(1,5);
 g.addEdge(2,5);
-g.addEdge(4,5);
-g.addEdge(5,8);
-g.addEdge(8,7);
-g.addEdge(7,3);
-g.addEdge(7,6);
-//g.showGraph();
+g.addEdge(3,4);
+g.showGraph();
 g.bfs(0);
-var vertex = 8;
+var vertex = 5;
 var source = 0;
 var paths = g.pathTo(source,vertex);
 g.showPath(paths);
